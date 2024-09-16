f123be993d480a527973133f6cf3410d77e79419
Princelad <ladprince1112@gmail.com>
2024-09-16 13:41:31 +0530

    minor changes

--- sortingAlgorithms.cpp
+++ sortingAlgorithms.cpp
@@ -55,9 +55,9 @@ void insertionSort(int arr[], int size) {
     for(int i = 1; i < size; i++) {
         key = arr[i];
         j = i - 1;
-        while(j > -1 && arr[j] > key) {
+        while(j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
         arr[j + 1] = key;
     }
